b1b3316ae120ad3abfdcc3b566794aef7132a065
Александра Блинова <blinovaao@yandex.ru>
2024-05-30 15:44:54 +0300

    исправляет поведение карточек

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-const item = document.querySelectorAll('.catalog__item');
-const closeButton = document.querySelectorAll('.item__close-button');
-const preimage = document.querySelector('.catalog__preimage');
+/* SLIDER */
 
-const items = Array.from(item);
-const buttons = Array.from(closeButton);
+const sliders = Array.from(document.querySelectorAll('.images-slider'));
+
+sliders.forEach(slider => {
+    const slides = Array.from(slider.querySelectorAll('.images-slider__item'));
+    const prevButton = slider.querySelector('.prev-button');
+    const nextButton = slider.querySelector('.next-button');
+    const slideCount = slides.length;
+    let slideIndex = 0;
+
+    // Устанавливаем обработчики событий для кнопок
+    prevButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide);
+    
+    // Функция для показа предыдущего слайда
+    function showPreviousSlide() {
+        slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+        updateSlider();
+    }
+    
+    // Функция для показа следующего слайда
+    function showNextSlide() {
+        slideIndex = (slideIndex + 1) % slideCount;
+        updateSlider();
+    }
+    
+    // Функция для обновления отображения слайдера
+    function updateSlider() {
+        slides.forEach((slide, index) => {
+            if (index === slideIndex) {
+                slide.style.display = 'flex';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    // Инициализация слайдера
+    updateSlider();
+})
+
+
+/* АНИМАЦИЯ КАРТОЧЕК */
+
+const items = Array.from(document.querySelectorAll('.catalog__item'));
 
 items.forEach(item => {
+    const closeButton = item.querySelector('.item__close-button');
+    const preimage = item.querySelector('.catalog__preimage');
+
     preimage.addEventListener('click', () => {
         item.classList.add('opened');
-    },
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            item.classList.remove('opened');
-        })
+    }),
+
+    closeButton.addEventListener('click', () => {
+        item.classList.remove('opened');
     })
-)
 })
 
 document.addEventListener('keydown', function(e) {
@@ -24,47 +65,3 @@ document.addEventListener('keydown', function(e) {
         })
     }
 })
-
-/* closeButton.addEventListener('click', () => {
-    item.classList.remove('opened');
-}) */
-
-/* SLIDER */
-
-// Получаем элементы слайдера
-const slider = document.querySelector('.images-slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('.images-slider__item'));
-const slideCount = slides.length;
-let slideIndex = 0;
-
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-    updateSlider();
-}
-
-// Функция для показа следующего слайда
-function showNextSlide() {
-    slideIndex = (slideIndex + 1) % slideCount;
-    updateSlider();
-}
-
-// Функция для обновления отображения слайдера
-function updateSlider() {
-    slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-        slide.style.display = 'flex';
-    } else {
-        slide.style.display = 'none';
-    }
-});
-}
-
-// Инициализация слайдера
-updateSlider();
